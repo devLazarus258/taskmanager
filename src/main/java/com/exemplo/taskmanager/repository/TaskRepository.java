554a3567ca13c747efd0534212adcbeb3d8cef11
Arlindo Lazaro <arlindolazaro202@gmail.com>
2025-08-10 17:34:31 +0200

    feat: criar o respositorio

---
+++ src/main/java/com/exemplo/taskmanager/repository/TaskRepository.java
@@ -0,0 +1,14 @@
+package com.exemplo.taskmanager.repository;
+
+import java.util.Optional;
+import java.util.List;
+
+import com.exemplo.taskmanager.model.Task;
+import com.exemplo.taskmanager.model.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository <Task, Long> {
+    List<Task> findByUser (User user);
+    Optional<Task> findByIdAdnUser(Long id, User ser);
+}
---
+++ src/main/java/com/exemplo/taskmanager/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.exemplo.taskmanager.repository;
+
+import java.util.Optional;
+import com.exemplo.taskmanager.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository <User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean exexistsByEmail(String email);
+}
