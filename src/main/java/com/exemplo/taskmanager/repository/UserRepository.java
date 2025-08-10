ebeb7632bca54acaebd46c9691b86f75888c27d6
Arlindo Lazaro <arlindolazaro202@gmail.com>
2025-08-10 17:35:47 +0200

    feat: modificar os componentes do sistema

--- src/main/java/com/exemplo/taskmanager/repository/UserRepository.java
+++ src/main/java/com/exemplo/taskmanager/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.exemplo.taskmanager.repository;
 
 import java.util.Optional;
 import com.exemplo.taskmanager.model.User;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository <User, Long> {
