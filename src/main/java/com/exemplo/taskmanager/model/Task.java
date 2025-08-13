e6bf39940098cf5e54a86fd9938d5132de901afe
Arlindo Lazaro <arlindolazaro202@gmail.com>
2025-08-13 02:37:31 +0200

    Atualiza modelo Task

--- src/main/java/com/exemplo/taskmanager/model/Task.java
+++ src/main/java/com/exemplo/taskmanager/model/Task.java
@@ -2,6 +2,7 @@ package com.exemplo.taskmanager.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import java.util.Date;
 
 @Entity
 @Table(name = "tasks")
@@ -24,4 +25,10 @@ public class Task {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @Builder.Default
+    private Date createdAt = new Date();
 }
+
